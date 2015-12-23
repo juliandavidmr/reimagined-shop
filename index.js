49cbe3d9f317a80582edabcc96cdf61467351956
Julian David <anlijudavid@hotmail.com>
2015-12-23 12:54:09 -0500

    Primera subida

---
+++ index.js
@@ -0,0 +1,12 @@
+var app = require('app')
+var BrowserWindow = require('browser-window')
+
+app.on('ready', function(){
+	var mainWindow = new BrowserWindow ({
+		width: 1400,
+		height: 800,
+		title: 'Shop'
+	})
+
+	mainWindow.loadUrl('file://' + __dirname + '/index.html')
+})
